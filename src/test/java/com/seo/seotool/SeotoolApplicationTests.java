c23956443d08eae1ebdedfe024dd013ac22a2db6
luxxmel <hoanglinhcntti@gmail.com>
2026-07-03 14:31:08 +0700

    Add files via upload

---
+++ src/test/java/com/seo/seotool/SeotoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seo.seotool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeotoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
