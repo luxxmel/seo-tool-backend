b1d0b15d0e35a11fc4adf9dd41497f21c0a28bfc
luxxmel <hoanglinhcntti@gmail.com>
2026-07-04 15:56:11 +0700

    Update AuditController.java

--- src/main/java/com/seo/seotool/controller/AuditController.java
+++ src/main/java/com/seo/seotool/controller/AuditController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/seo")
+@RequestMapping("/api/audit")
 @CrossOrigin(
         origins = {
                 "http://localhost:5173",
