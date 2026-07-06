310b8a0a4698aa49ff766627d7fc7ee143668b6f
luxxmel <hoanglinhcntti@gmail.com>
2026-07-06 15:42:09 +0700

    Delete src directory

--- src/main/java/com/seo/seotool/dto/UrlRequest.java
+++
@@ -1,9 +0,0 @@
-package com.seo.seotool.dto;
-
-import lombok.Data;
-import java.util.List;
-
-@Data // Tự động tạo getter, setter nhờ Lombok
-public class UrlRequest {
-    private List<String> urls;
-}
